4431d32b65e604ba4aacd405b30d8080b6c28d5e
Jan Seeger <jan@alphadev.net>
2015-04-13 22:09:05 +0200

    jslint clean.

--- js/jeopardy.js
+++ js/jeopardy.js
@@ -1,6 +1,15 @@
-var game = {}
+/*jslint browser: true*/
+/*global $, jQuery, alert, FileReader */
+
+var game = {};
+var daily = {};
+var prompt = {};
+
 game.init = function () {
-    if (game.gamedata == null) return;
+    'use strict';
+    if (game.gamedata === null) {
+        return;
+    }
 
     $('#game').fadeIn(1000);
     $('#options').hide();
@@ -13,57 +22,143 @@ game.init = function () {
             "points": 0
         });
     });
-    game.createScoreboard()
+    game.createScoreboard();
     game.current_points = 0;
-}
+};
 
 game.createScoreboard = function () {
-    var content = "<table cellspacing=10><tbody><tr>";
-    for (var i = 0; i < game.teams.length; i++) {
-        content += "<th><h3>" + game.teams[i]["name"] + "</h3></th>";
+    'use strict';
+
+    var i, content;
+
+    content = "<table cellspacing=10><tbody><tr>";
+    for (i = 0; i < game.teams.length; i = i + 1) {
+        content += "<th><h3>" + game.teams[i].name + "</h3></th>";
     }
+
     content += "</tr><tr>";
-    for (var i = 0; i < game.teams.length; i++) {
+    for (i = 0; i < game.teams.length; i = i + 1) {
         content += "<td><h3 id='team" + i + "'>0</h3><input class='add-points' onclick='game.addPoints(" + i + ")' value='+' type='button' /><input class='subtract-points' onclick='game.subtractPoints(" + i + ")' type='button' value='-' /></td>";
     }
+
     content += "</tr></tbody></table>";
     $('#stats').html(content);
-}
+};
 
 game.addPoints = function (team) {
+    'use strict';
     game.updatePoints(team, game.current_points);
-}
+};
 
 game.subtractPoints = function (team) {
-    var points = 0 - game.current_points;
+    'use strict';
+    var points = -game.current_points;
     game.updatePoints(team, points);
-}
+};
 
 game.updatePoints = function (team, diff) {
-    game.teams[team]['points'] += diff;
+    'use strict';
+    game.teams[team].points += diff;
 
-    $('#team' + team).html(game.teams[team]['points']);
+    $('#team' + team).html(game.teams[team].points);
     $('#' + game.current_questionID).addClass("dirty")
         .unbind('mouseover')
         .unbind('mouseout');
 
     game.current_points = 0;
-}
+};
 
 game.loadFile = function (onload) {
+    'use strict';
     if (typeof window.FileReader !== 'function') {
         return;
     }
 
-    var input = document.getElementById('fileinput');
+    var fr, input = document.getElementById('fileinput');
     if (input && input.files && input.files[0]) {
-        var fr = new FileReader();
+        fr = new FileReader();
         fr.onload = onload;
         fr.readAsText(input.files[0]);
     }
-}
+};
+
+game.extractFromId = function (identifier, index) {
+    'use strict';
+    var retval, regexp = /c([0-9])r([0-9])/g;
+    retval = regexp.exec(identifier);
+    return retval[index];
+};
+
+prompt.show = function (field) {
+    'use strict';
+    game.current_points = parseInt(field.find('h3').text(), 10);
+    game.current_questionID = field.attr("id");
+    var column, row, dataset, question, answer;
+    column = game.extractFromId(field.attr("id"), 1) - 1;
+    row = game.extractFromId(field.attr("id"), 2) - 1;
+    dataset = game.gamedata.data[column].data[row];
+
+    $('#game').hide();
+
+    if (dataset.hasOwnProperty("daily")) {
+        if (game.points_bet) {
+            game.current_points = parseInt(game.points_bet, 10);
+            game.points_bet = null;
+        } else {
+            daily.show(field);
+            return;
+        }
+    }
+
+    question = prompt.loadImage(dataset.question);
+    answer = prompt.loadImage(dataset.answer);
+
+    $('#question').hide();
+    $('#prompt').fadeIn(1000);
+    $('#answer').html(question);
+    $('#question').html(answer);
+    if ($('#question').html().length === 0) {
+        $('#correct-response').hide();
+    } else {
+        $('#correct-response').show();
+    }
+};
+
+prompt.loadImage = function (image) {
+    'use strict';
+    var prefix = game.gamedata.directory;
+    if (image.substring(0, 6) === "image:") {
+        return "<img src=\"data/" + prefix + "/" + image.substring(6) + "\">";
+    }
+    return image;
+};
+
+prompt.hide = function () {
+    'use strict';
+    $('#prompt').hide();
+    $('#game').show();
+};
+
+prompt.showQuestion = function () {
+    'use strict';
+    $('#question').fadeIn(1000);
+};
+
+daily.show = function (field) {
+    'use strict';
+    $('#prompt').hide();
+    $('#daily').show();
+
+    $('#bet_submit').click(function () {
+        game.points_bet = $("#bet").val();
+        game.current_points = game.points_bet;
+        prompt.show(field);
+        $('#daily').hide();
+    });
+};
 
 $(document).ready(function () {
+    'use strict';
     $('tbody tr td').each(function () {
         $(this).addClass('cell')
             .addClass('clean')
@@ -75,28 +170,32 @@ $(document).ready(function () {
     $('textarea.edit').focus(function () {
         $(this).addClass('active');
         var val = $(this).val();
-        if (val == "Enter Category" || val == "Enter Title")
+        if (val === "Enter Category" || val === "Enter Title") {
             this.select();
+        }
     }).blur(function () {
         $(this).removeClass('active');
     }).autogrow();
 
     $('.clean').mouseover(function () {
-        $(this).addClass('ie-hack')
+        $(this).addClass('ie-hack');
     }).mouseout(function () {
-        $(this).removeClass('ie-hack')
-    })
+        $(this).removeClass('ie-hack');
+    });
 
     $('#fileinput').change(function () {
         game.loadFile(function (e) {
-            var data = $.parseJSON(e.target.result);
-            $('#title').html(data["name"]);
-            $('#gametitle').html(data["name"]);
-			var i = 0;
+            var data, j;
+            data = $.parseJSON(e.target.result);
+            $('#title').html(data.name);
+            $('#gametitle').html(data.name);
+            j = 0;
             $('thead').find('th:not(#gametitle)').each(function () {
-                var id = $(this).attr("id");
-                var label = data["data"][i++]["name"];
-                $(this).html(label)
+                var id, label;
+                id = $(this).attr("id");
+                label = data.data[j].name;
+                $(this).html(label);
+                j = j + 1;
             });
             game.gamedata = data;
         });
@@ -106,73 +205,3 @@ $(document).ready(function () {
         $("<input type=\"text\" name=\"team[]\" class=\"team\" />").appendTo("#teams");
     });
 });
-
-game.extractFromId = function(identifier, index) {
-	var regexp = /c([0-9])r([0-9])/g;
-	var retval = regexp.exec(identifier);
-	return retval[index];
-}
-
-var prompt = {}
-prompt.show = function (field) {
-    game.current_points = parseInt(field.find('h3').text());
-    game.current_questionID = field.attr("id");
-    var column = game.extractFromId(field.attr("id"), 1) -1;
-    var row = game.extractFromId(field.attr("id"), 2) -1;
-    var dataset = game.gamedata["data"][column]["data"][row];
-
-    $('#game').hide();
-
-    if ("daily" in dataset) {
-        if (game.points_bet) {
-            game.current_points = parseInt(game.points_bet);
-            game.points_bet = null;
-        } else {
-            daily.show(field);
-            return;
-        }
-    }
-
-    var question = prompt.loadImage(dataset['question']);
-    var answer = prompt.loadImage(dataset['answer']);
-
-    $('#question').hide();
-    $('#prompt').fadeIn(1000);
-    $('#answer').html(question);
-    $('#question').html(answer);
-    if ($('#question').html().length == 0)
-        $('#correct-response').hide();
-    else
-        $('#correct-response').show();
-}
-
-prompt.loadImage = function (image) {
-    var prefix = game.gamedata['directory'];
-    if (image.substring(0, 6) == "image:") {
-        return "<img src=\"data/" + prefix + "/" + image.substring(6) + "\">";
-    }
-    return image;
-}
-
-prompt.hide = function () {
-    $('#prompt').hide();
-    $('#game').show();
-}
-
-prompt.showQuestion = function () {
-    $('#question').fadeIn(1000)
-}
-
-var daily = {}
-daily.show = function (field) {
-    $('#prompt').hide();
-    $('#daily').show();
-
-    $('#bet_submit').click(function () {
-        game.points_bet = $("#bet").val();
-        game.current_points = game.points_bet;
-        prompt.show(field);
-        $('#daily').hide();
-    });
-}
-
